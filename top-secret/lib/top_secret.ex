fa65b557b604f5003cdfc035eccfb194ae2af39c
Rupak Lamsal <rupak@mojotech.com>
2022-10-07 15:51:31 -0400

    update: top secret solution
    more pattern matching and pipe

--- top-secret/lib/top_secret.ex
+++ top-secret/lib/top_secret.ex
@@ -5,20 +5,21 @@ defmodule TopSecret do
 
   def decode_secret_message_part({atom, _, func} = ast, acc) when atom in [:def, :defp] do
     {f_name, arity} = func_name_and_arity(func)
-
-    len = if is_nil(arity), do: 0, else: arity |> length()
-    name = f_name |> to_string() |> String.split_at(len) |> elem(0)
-    {ast, [name | acc]}
+    {ast, [f_name |> to_string() |> String.split_at(arity) |> elem(0) | acc]}
   end
 
   def decode_secret_message_part(ast, acc), do: {ast, acc}
 
-  defp func_name_and_arity([{:when, _, [{f_name, _, arity}, _]}, _]), do: {f_name, arity}
-  defp func_name_and_arity([{f_name, _, arity}, _]), do: {f_name, arity}
+  defp func_name_and_arity([{:when, _, [{f_name, _, arity}, _]}, _]), do: {f_name, length(arity)}
+  defp func_name_and_arity([{f_name, _, nil}, _]), do: {f_name, 0}
+  defp func_name_and_arity([{f_name, _, arity}, _]), do: {f_name, length(arity)}
 
   def decode_secret_message(string) do
-    ast = string |> to_ast()
-    {_, acc} = Macro.prewalk(ast, [], fn (asts, acc) -> decode_secret_message_part(asts, acc) end)
-    acc |> Enum.reverse() |> Enum.join()
+    string
+    |> to_ast()
+    |> Macro.prewalk([], &(decode_secret_message_part(&1, &2)))
+    |> elem(1)
+    |> Enum.reverse()
+    |> Enum.join()
   end
 end
